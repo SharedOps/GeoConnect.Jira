6d1737734935ac6d3674379870750bdff39b2c8e
SharedOps <manikanta.mundru@gmail.com>
2018-11-08 19:30:59 +0200

    New project added for project management

---
+++ GeoConnect.Application/Providers/JiraApplicationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeoConnect.Application.Interfaces;
+
+namespace GeoConnect.Application.Providers
+{
+    public class JiraApplicationService : IJiraApplicationService
+    {
+
+    }
+}
---
+++ GeoConnectJiraServices/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GeoConnectJiraServices.WebApiApplication" Language="C#" %>
